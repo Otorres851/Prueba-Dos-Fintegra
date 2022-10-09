8951f5bc04780a8ff1af2a4a6fca3c7fbedb735a
osharley38 <osharley38@gmail.com>
2022-10-09 17:35:54 -0500

    Modificamos formulario y agregamos estilos al componente SearchAge

--- src/Components/SearchAge.jsx
+++ src/Components/SearchAge.jsx
@@ -6,14 +6,14 @@ const SearchAge = () => {
             <form>
                 <label className="form-label col-sm-2">Insert Name</label>
                 <input
-                    type="text" placeholder='Search Age' required
+                    type="text" placeholder='Name' required
                     class="form-control form-control-sm col-md-12" />
                 
-                <h2>Optional</h2> 
+                <h2 className="option">Optional</h2> 
                 
                 <label className="form-label col-sm-2">Insert Localization</label>
                 <input
-                    type="text" placeholder='Search Age'
+                    type="text" placeholder='Localization'
                     class="form-control form-control-sm col-md-12" />
                 <div>
                     <button type={'submit'} className="btn col-sm-6">Submit</button>
