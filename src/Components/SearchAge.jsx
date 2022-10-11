5192541b3a6d1393d04f1b621f185a31b0d42406
osharley38 <osharley38@gmail.com>
2022-10-10 22:11:01 -0500

    Agregamos funcionalidad al segundo input y luego editamos html y css

--- src/Components/SearchAge.jsx
+++ src/Components/SearchAge.jsx
@@ -5,7 +5,6 @@ const SearchAge = () => {
     const [guessedAge, setGuessedAge] = useState(undefined);
     const [loading, setLoading] = useState(false);
 
-
     const handleChange = (e) => {
         setName(e.target.value)
 
@@ -14,9 +13,9 @@ const SearchAge = () => {
     const requestGuessAge = async (name) => {
         const response = await fetch(`https://api.agify.io?name=${name}`);
         const { age } = await response.json();
-        return age;
+        return age;        
     }
-
+    
     useEffect(() => {
         if (!name) return;
         setLoading(true);
@@ -26,6 +25,31 @@ const SearchAge = () => {
     }, [name]);
 
 
+
+    /*Country*/
+    const [nam, setNam] = useState("");
+    const [guessedCountry, setGuessedCountry] = useState(undefined);
+    const [load, setLoad] = useState(false);
+
+    const handleChangeName = (e) => {
+        setNam(e.target.value)
+
+    };
+
+    const requestGuessCountry = async (name) => {
+        const response = await fetch(`https://api.agify.io?name=${name}&country_id=US`);
+        const { country_id } = await response.json();
+        return country_id;        
+    }
+
+    useEffect(() => {
+        if (!nam) return;
+        setLoad(true);
+        requestGuessCountry(nam)
+            .then(setGuessedCountry)
+            .finally(() => setLoad(false));
+    }, [nam]);
+    
     return (
         <div className='container'>
             <form>
@@ -40,7 +64,8 @@ const SearchAge = () => {
                 <label className="form-label col-sm-2">Insert Name For Localization</label>
                 <input
                     type="text" placeholder='Name For Localization'
-                    className="form-control form-control-sm col-md-12" />
+                    className="form-control form-control-sm col-md-12" value={nam} 
+                    onChange={handleChangeName}/>
                 <div>
                     <p className="AgeText">
                         <AgeText
@@ -51,21 +76,42 @@ const SearchAge = () => {
                     </p>
                 </div>
 
+                <div>
+                    <p className="CountryText">
+                        <CountryText
+                            name={nam}
+                            country_id={guessedCountry}
+                            load={load}
+                        />
+                    </p>
+                </div>
+
             </form>
         </div>
     )
 
+    /*AgeText & CountryText*/ 
+
     function AgeText({ name, age, loading }) {
         if (!name) return "";
         if (loading) return `Hello ${name}! Your age is ...`;
-        if (typeof age === "number") {
+        if (typeof age === 'number') {
             return `Hello ${name}! Your age is ${age}`;
         } else {
             return `Sorry ${name}! I could not guess your age!`;
         }
     }
 
-
+    function CountryText({ name, country_id, load }) {
+        if (!name) return "";
+        if (load) return `Hello ${name}! Your country is ...`;
+        if (typeof country_id === 'string') {
+            return `Hello ${name}! Your country is ${country_id}`;
+        } else {
+            return `Sorry ${name}! I could not guess your country!`;
+        }
+    }
+   
 }
 
 export default SearchAge;
